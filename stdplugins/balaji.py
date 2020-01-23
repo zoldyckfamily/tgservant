3ff07211a5a6d7436ff4101b45bc12a0487d232c
Abdullah Al Anas Himel <43927831+aaahimel@users.noreply.github.com>
2020-01-23 16:53:07 +0600

    Update balaji.py

--- stdplugins/balaji.py
+++ stdplugins/balaji.py
@@ -22,15 +22,15 @@ async def _(event):
 
     input_str = event.pattern_match.group(1)
 
-    if input_str == "balaji":
+    if input_str == "update":
 
         await event.edit(input_str)
 
         animation_chars = [
         
-            "`Your bot is running\n\nTelethon version:` 1.9.0\n`Python:` 3.7.3\n`User:` @r4v4n4\n`Database Status: Databases functioning normally!`",
+            "`Your bot is running\n\nTelethon version:` 1.9.0\n`Python:` 3.8.3\n`User:` @whi5tLe_bL0w3R\n`Database Status: Databases functioning normally!`",
             "`Connecting To github.com...`",
-            "`Deleting Baalaji Repo....`",
+            "`Deleting Garbage Repo....`",
             "`Forking Uniborg... 0%\n\n⬜️⬜️⬜️⬜️⬜️⬜️⬜️⬜️⬜️⬜️⬜️⬜️⬜️⬜️\n\nFile Size: 0 MiB / 108.7 MiB`",
             "`Forking Uniborg... 4%\n\n⬛️⬜️⬜️⬜️⬜️⬜️⬜️⬜️⬜️⬜️⬜️⬜️⬜️⬜️\n\nFile Size: 22 MiB / 108.7 MiB`",
             "`Forking Uniborg... 8%\n\n⬛️⬛️⬜️⬜️⬜️⬜️⬜️⬜️⬜️⬜️⬜️⬜️⬜️⬜️\n\nFile Size: 48 MiB / 108.7 MiB`",    
@@ -39,8 +39,8 @@ async def _(event):
             "`Forking Uniborg... 52%\n\n⬛️⬛️⬛️⬛️⬛️⬛️⬛️⬛️⬛️⬜️⬜️⬜️⬜️⬜️\n\nFile Size: 90.7 MiB / 108.7 MiB `",
             "`Forking Uniborg... 84%\n\n⬛️⬛️⬛️⬛️⬛️⬛️⬛️⬛️⬛️⬛️⬛️⬛️⬜️⬜️\n\nFile Size: 100.7 MiB / 108.7 MiB `",
             "`Forking Uniborg... 100%\n\n⬛️⬛️⬛️⬛️⬛️⬛️⬛️⬛️⬛️⬛️⬛️⬛️⬛️⬛️\n\nFile Size: 108.7 MiB / 108.7 MiB\n\nTask Completed... `",
-            "`Fork Deploying...`\n\n@UniBorg ( `Custom Built By` @r4v4n4 ) \n`Verified Account:` ☑️\n`Official Website:` https://ravanaisdrunk.site.live\n\n`Python` `Loading...`\n[GCC 7.4.0]\n`Telethon` `Loading...`\n\n`Custom Built Fork:` `Loading...`",
-            "`Fork Deployed...`\n\n@UniBorg ( `Custom Built By` @r4v4n4 ) \n`Verified Account:` ✅\n`Official Website:` https://ravanaisdrunk.site.live\n\n`Python` 3.7.4 (default, Sep 12 2019, 01:19:52)\n[GCC 7.4.0]\n`Telethon` 1.8.0\n\n`Custom Built Fork:` https://github.com/ravana69/PornHub"
+            "`Fork Deploying...`\n\n@TelegramBot ( `Custom Built By` @whi5tLe_bL0w3R ) \n`Verified Account:` ☑️\n`Official Website:` https://himel.best\n\n`Python` `Loading...`\n[GCC 7.4.0]\n`Telethon` `Loading...`\n\n`Custom Built Fork:` `Loading...`",
+            "`Fork Deployed...`\n\n@Telegrambot( `Custom Built By` @whi5tLe_bL0w3R ) \n`Verified Account:` ✅\n`Official Website:` https://himel.best\n\n`Python` 3.8.4 (default,2020, 01:19:52)\n[GCC 7.4.0]\n`Telethon` 1.8.0\n\n`Custom Built Fork:`Loading...` "
         ]
 
         for i in animation_ttl:
