d406f3c58514991f91c46e435f5c34d127ab0a74
zoldyckfamily <60518557+zoldyckfamily@users.noreply.github.com>
2020-01-31 18:53:11 +0000

    Update app.py

--- stdplugins/app.py
+++ stdplugins/app.py
@@ -59,7 +59,7 @@ async def apkr(e):
         app_details += "\n<code>Rating :</code> "+app_rating.replace("Rated ", "⭐ ").replace(" out of ", "/").replace(" stars", "", 1).replace(" stars", "⭐ ").replace("five", "5")
         app_details += "\n<code>Features :</code> <a href='"+app_link+"'>View in Play Store</a>"
         app_details += "\n\n<b>Download : </b> <a href='tg://need_update_for_some_feature'>Request_Here</a>"
-        app_details += "\n\n===> @whi5tLe_bL0w3R <==="
+       
         await e.edit(app_details, link_preview = True, parse_mode = 'HTML')
     except IndexError:
         await e.edit("No result found in search. Please enter **Valid app name**")
