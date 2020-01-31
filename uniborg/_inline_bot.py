963ae145cc2e8a9254e5be4e89006bf73cbf2a07
zoldyckfamily <60518557+zoldyckfamily@users.noreply.github.com>
2020-01-31 21:48:27 +0000

    Update _inline_bot.py

--- uniborg/_inline_bot.py
+++ uniborg/_inline_bot.py
@@ -66,41 +66,41 @@ if Config.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
         builder = event.builder
         result = None
         query = event.text
-        if event.query.user_id == borg.uid and query.startswith("@UniBorg"):
+        if event.query.user_id == borg.uid and query.startswith("@meanii"):
             rev_text = query[::-1]
             buttons = paginate_help(0, borg._plugins, "helpme")
             result = builder.article(
-                "© @UniBorg",
+                "© @meanii",
                 text="{}\nℂ𝕦𝕣𝕣𝕖𝕟𝕥𝕝𝕪 𝕃𝕠𝕒𝕕𝕖𝕕 ℙ𝕝𝕦𝕘𝕚𝕟𝕤: {}".format(
                     query, len(borg._plugins)),
                 buttons=buttons,
                 link_preview=True
             )
-        elif query.startswith("choot"):
+        elif query.startswith("meanii"):
             result = builder.article(
-                "@r4v4n4: Bhagwaan Sabko GF De",
-                text=f"[Choot](https://telegra.ph/file/019a2eab3d66d39c92a75.mp4)",
+                "@meanii: Noob/xD 👨🏻‍💻",
+                text=f"[meanii](https://telegra.ph/file/88a9fc26e29332495f8a7.mp4)",
                 buttons=[],
                 link_preview=True
             )
         else:
             result = builder.article(
-                "© @UniBorg",
-                text="""@r4v4n4 **( Custom Built By** @r4v4n4 **)** 
+                "© @meanii",
+                text="""@meanii **( Custom Built By** @meanii **)** 
 **Verified Account:** ✅
-**Official Website:** https://ravanaisdrunk.site.live [⠀](https://telegra.ph/file/b0604ea53360cd3858ec5.mp4)
+**Official Website:** https://gizmostuffgstuff.blogspot.com [⠀](https://telegra.ph/file/88a9fc26e29332495f8a7.mp4)
 
-**Pithun 3.7.4 (default, Sep 12 2019, 01:19:52)** 
+**Pithun 3.7.4 (default, feb 1 2020, 01:19:52)** 
 **[GCC 7.4.0]**
 **Talethrun 1.10.6**
 
-**Custom Built Fork:** https://github.com/ravana69/Pornhub""",
+**Custom Built Fork:** https://github.com/mitshuhataki/kaito/""",
                 buttons=[
-                    [custom.Button.url("👤Contact Creator👤", "https://telegram.dog/r4v4n4"), custom.Button.url(
-                        "📼Ravana Audio Memes📼", "https://t.me/tgaudiomemes")],
-                    [custom.Button.url("👨‍💻Source Code👨‍💻", "https://github.com/ravana69/Pornhub"), custom.Button.url(
-                        "❕❗Deploy Me❗❕", "https://dashboard.heroku.com/new?button-url=https%3A%2F%2Fgithub.com%2Fravana69%2FPornHub%2F&template=https%3A%2F%2Fgithub.com%2Fravana69%2FPornHub%2F")],
-                    [custom.Button.url("🔰Update Fork🔰", "tg://need_update_for_some_feature"), custom.Button.url(
+                    [custom.Button.url("👤Contact Creator👤", "https://telegram.dog/meanii"), custom.Button.url(
+                        "📼Instragram📼", "https://www.instagram.com/mitshuhataki/")],
+                    [custom.Button.url("👨‍💻Source Code👨‍💻", "https://gizmostuffgstuff.blogspot.com/"), custom.Button.url(
+                        "❕❗Deploy Me❗❕", "https://dashboard.heroku.com/new?button-url=https%3A%2F%2Fgithub.com%2Fzoldyckfamily%2Fzoldyck&template=https%3A%2F%2Fgithub.com%2Fzoldyckfamily%2Fzoldyck")],
+                    [custom.Button.url("🌚Youtube Channel🌚", "https://www.youtube.com/channel/UC6FsUGrV-fJtWqRykYfJscg"), custom.Button.url(
                         "✳️Fork Boost✳️", "tg://some_unsupported_feature"), custom.Button.url(
                         "♻️Refresh Heroku♻️", "tg://chutiya")]
                 ],
@@ -121,7 +121,7 @@ if Config.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
             # https://t.me/TelethonChat/115200
             await event.edit(buttons=buttons)
         else:
-            reply_pop_up_alert = "⚠️ Warning: Don't Press Any Buttons ⚠️\n\nCustom Fork: https://github.com/ravana69/Pornhub\n\n\nNote: Bas kar BetiChod, Maa Ke Laude, Madarchod"
+            reply_pop_up_alert = "⚠️ Warning: Don't Press Any Buttons ⚠️"
             await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
 
     @tgbot.on(events.callbackquery.CallbackQuery(  # pylint:disable=E0602
@@ -139,7 +139,7 @@ if Config.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
             # https://t.me/TelethonChat/115200
             await event.edit(buttons=buttons)
         else:
-            reply_pop_up_alert = "Please get your own @UniBorg, and don't edit my messages!"
+            reply_pop_up_alert = "Please get your own Bot, and don't edit my messages!"
             await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
 
 
